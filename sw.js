0ef60e1c35ec4f96fb62d8343cdad53db5485ea4
TheBugFather <erbeawebird@gmail.com>
2026-06-07 02:51:18 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,45 @@
+const CACHE_NAME = 'map-mbg-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Service Worker & Cache Asset Utama
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('map-mbg-store').then((cache) => cache.addAll([
-      './index.html',
-      './manifest.json',
-      './icon.png'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
   );
 });
 
+// Aktivasi & Pembersihan Cache Lama
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Strategi Cache: Network First, Fallback to Cache
 self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    fetch(e.request).then((res) => {
+      const resClone = res.clone();
+      caches.open(CACHE_NAME).then((cache) => {
+        cache.put(e.request, resClone);
+      });
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
