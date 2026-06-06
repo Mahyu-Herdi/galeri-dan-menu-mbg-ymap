7c9243e2cd14cf46c33b926ddee414333420ad8d
TheBugFather <erbeawebird@gmail.com>
2026-06-06 15:20:02 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('map-mbg-store').then((cache) => cache.addAll([
+      './index.html',
+      './manifest.json',
+      './icon.png'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
